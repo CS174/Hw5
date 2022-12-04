61aef8405f1f97efd569c0be15bc54259e416557
Michael Ruiz <michael@mruiz.dev>
2022-12-03 22:20:07 -0800

    delete src/config

--- src/config/Config.js
+++
@@ -1,9 +0,0 @@
-// Change the values of your database
-const MYSQLUSERNAME = "root";
-const MYSQLPASSWORD = "jaibholenath";
-const MYSQLPORT = "3306"
-const DATABASE = "CS174"
-const SOCKETPATH = "/var/run/mysqld/mysqld.sock"
-const EXPRESSPORT = 8888;
-
-module.exports = {MYSQLUSERNAME, MYSQLPASSWORD, EXPRESSPORT}
