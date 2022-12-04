fe12b36d3a4a0d02ea2d71e32e80b3ca2f9a1e55
Michael Ruiz <michael@mruiz.dev>
2022-12-03 21:29:00 -0800

    working on sql

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+app.use(cors({origin: "http://localhost:3000"}));
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const pool = require("../database");
+const pool = require("../database").pool;
 
 
 /* GET home page. */
 router.post('/create', async function(req, res, next) {
     const body = req.body;
     console.log(body)
-    let sql = 'SELECT * FROM inventory';
+    const sql = `INSERT INTO users (email, name, password) VALUES ('${body.email}', '${body.name}', '${body.password}');`
 
     pool.getConnection((err, connection) => {
         if (err) throw err;
@@ -15,9 +15,13 @@ router.post('/create', async function(req, res, next) {
         connection.query(sql, (err, result) => {
             if (err) throw err;
             console.log(result);
-            res.send('Inventory received via pool.');
-
-            connection.release();
+            const sql_answers = `INSERT INTO answers (q1, q2, q3, q4, q5, q6, q7, q8, q9, q10) 
+            VALUES ('${body.q1}', '${body.q2}', '${body.q3}', '${body.q4}', '${body.q5}', 
+            '${body.q6}', '${body.q7}', '${body.q8}', '${body.q9}', '${body.q10}',)`;
+            connection.query(sql, (err, result) => {
+                res.send(result);
+                connection.release();
+            })
         });
     });
     // res.redirect("/")
