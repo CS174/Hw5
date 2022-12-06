986240f4b7da18755b21e376fbcda3b6a26eea38
Aadit Dubey <dubeyaadit@gmail.com>
2022-12-05 23:26:23 -0800

    done

--- src/Pages/DateCards.js
+++ src/Pages/DateCards.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React , {useState,useEffect}from 'react'
 
-const DateCards = ({name}) => {
+const DateCards = ({name,email,userData,setUserData,answers}) => {
+  const [showEmail, setShowEmail] = useState(false)
+  const [vector, setVector] = useState(0)
+  const showVector = ()=>{
+    
+  }
+
+  useEffect(() => {
+    let sum=0
+    for(let i=0;i<answers.length;i++){
+      if(answers[i]==1) sum++
+    }
+    setVector(sum/10)
+  }, [])
+  
   return (
     <div>
         <br/>
         <span>{name} </span>
-        <button>Setup a Date</button>
+        <span> Similarity {parseInt(Math.abs(vector+userData)*50)}% </span>
+        <button onClick={()=>setShowEmail(!showEmail)}>Setup a Date</button>
+        <button onClick ={()=>{if(userData<0.9){setUserData(userData+0.1)}}}>More like this one</button>
+        <button onClick ={()=>{if(userData>0.1){setUserData(userData-0.1)}}}>Less like this one</button>
+        {showEmail&&<span>{email}</span>}
+
         <br/>
     </div>
   )
